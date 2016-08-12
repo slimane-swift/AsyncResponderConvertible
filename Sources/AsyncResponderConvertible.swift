e1419bff9ea8bfa0e2ec32e1414542ad7ca285e9
noppoman <yuki@miketokyo.com>
2016-08-12 22:29:37 +0900

    @escaping

--- Sources/AsyncResponderConvertible.swift
+++ Sources/AsyncResponderConvertible.swift
@@ -8,17 +8,19 @@
 
 import S4
 
+private let key = "Slimane.Internal.CustomResponder"
+
 public protocol AsyncResponderConvertible {
-    func respond(_ response: Response, result: ((Void) throws -> Response) -> Void)
+    func respond(_ response: Response, result: @escaping ((Void) throws -> Response) -> Void)
 }
 
 extension Response {
     public var customResponder: AsyncResponderConvertible? {
-        return self.storage["Slimane.Internal.CustomResponder"] as? AsyncResponderConvertible
+        return self.storage[key] as? AsyncResponderConvertible
     }
 
     public init (version: Version = Version(major: 1, minor: 1), status: Status = .ok, headers: Headers = [:], cookieHeaders: Set<String> = [], custom customResponder: AsyncResponderConvertible){
         self.init(version: version, status: status, headers: headers, cookieHeaders: cookieHeaders, body: .buffer(Data()))
-        self.storage["Slimane.Internal.CustomResponder"] = customResponder
+        self.storage[key] = customResponder
     }
 }
